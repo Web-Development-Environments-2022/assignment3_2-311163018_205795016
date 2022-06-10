fc83e7ec0f89c43733aaad23234c6ded9739f710
hodisan94 <dhodisan@gmail.com>
2022-06-10 15:42:56 +0300

    trying

---
+++ routes/utils/home_utils.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const api_domain = "https://api.spoonacular.com/recipes";
+
+
+async function getRandomRecipes() {
+    let res = await axios.get(`${api_domain}/random`, {
+        params: {
+            apiKey: process.env.spooncular_apiKey,
+            number: 3,
+        },
+    })
+    return res;
+}
+
+exports.getRandomRecipes = getRandomRecipes;
