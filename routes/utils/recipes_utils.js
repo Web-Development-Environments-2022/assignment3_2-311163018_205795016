8571d029fff3c5c4367c31395812cf4b70f68e4a
omrim <omriman11@gmail.com>
2022-06-12 21:19:44 +0300

    fixed the world but I forgot what i fixed ;-(

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
-
+const DButils = require("./DButils");
 
 
 /**
@@ -264,10 +264,90 @@ async function getRecipesPreview(recipes_id_arr,keyToSort) {
 
 }
 
+/**
+ * make preview to recipes that on OUR DB (in the table newrecipes)
+ * @param {*} recipes_id_arr 
+ * @param {*} keyToSort 
+ * @returns 
+ */
+async function getMyRecipesPreview(recipes_id_arr,keyToSort) {
+    // let number_of_recipes = recipes_id_arr.length;
+    let recipes_preview_array = [];
+    for (let i=0;i<recipes_id_arr.length;i++) {
+        let recipe_info = await DButils.execQuery(`select * from newrecipes where id='${recipes_id_arr[i]}'`);
+        //JSON.stringify(recipe_info) - convert "RowDataPacket" to js object
+        let string_obj = JSON.stringify(recipe_info);
+        //JSON.parse(string_obj) convert it to array that in index 0 the row we want exists
+        recipes_preview_array.push(JSON.parse(string_obj)[0]);
+    }
+    final_recipes_preview_array =[];
+    let number_of_recipes = recipes_preview_array.length;
+    for (let i=0;i<number_of_recipes; i++) {
+        // let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipes_preview_array[i];
+            //not sure if we want to add recipe ID, or maybe its need to be client problem ?
+        try{
+            let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree,instructions } = recipes_preview_array[i];
+            final_recipes_preview_array.push({
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,
+            instructions: instructions,
+        })
+        } catch {
+            let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipes_preview_array[i];
+            final_recipes_preview_array.push({
+                id: id,
+                title: title,
+                readyInMinutes: readyInMinutes,
+                image: image,
+                popularity: aggregateLikes,
+                vegan: vegan,
+                vegetarian: vegetarian,
+                glutenFree: glutenFree,
+            })
+        }
+
+    }
+    if (keyToSort == "readyInMinutes"){
+        try {
+            final_recipes_preview_array.sort(function(a,b) {
+                return a.readyInMinutes - b.readyInMinutes
+            })
+        } catch {
+            console.log("sort by readyInMinutes Failed - return unsorted list of recipes")
+            return final_recipes_preview_array;
+        }
+    } else {
+        try {
+            final_recipes_preview_array.sort(function(a,b) {
+                var keyA = a.popularity;
+                var keyB = b.popularity;
+                if (keyA < keyB) {
+                 return -1;
+                }    
+                if (keyA > keyB) {
+                    return 1;
+                }
+                return 0;
+            })
+        } catch {
+            console.log("sort by popularity Failed - return unsorted list of recipes")
+            return final_recipes_preview_array;
+        }        
+    }
+    return final_recipes_preview_array;
+
+}
 
 exports.getRecipeDetails = getRecipeDetails;
 exports.getRecipesByQuery = getRecipesByQuery;
 exports.getRecipesPreview = getRecipesPreview;
+exports.getMyRecipesPreview = getMyRecipesPreview;
 
 
 
