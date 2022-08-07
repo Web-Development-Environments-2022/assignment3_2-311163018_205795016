96efa00812df16af19326140bcb993d0a5c2a6cd
hodisan94 <dhodisan@gmail.com>
2022-08-07 17:03:37 +0300

    changed about

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/", (req, res) => res.send("The legandery team of two notorius Full - Stack developers A.K.A Daniel 'the code chewer' Hodisan and Omri 'the debugger' Man."));
+router.get("/", (req, res) => res.send("The legandery team of two notorius Full - Stack developers A.K.A Daniel 'the code chewer' Hodisan and Omri 'the debugger' Man.\n Want to know  more about us? \n Check our other projects."));
 
 
 module.exports = router;
